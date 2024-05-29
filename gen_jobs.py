1893eaff8cacbee098f84efd24cada4f728b9140
Severin <s.bratus@student.tudelft.nl>
2024-05-29 08:52:56 +0200

    Gen for gpu

--- gen_jobs.py
+++ gen_jobs.py
@@ -1,16 +1,20 @@
 import sys
 import os
+import random
+
+
+gpus_per_task_one = "#SBATCH --gpus-per-task=1"
 
 template = """#!/bin/sh
 
-#SBATCH --job-name=N{n:03}K{k:03}
-#SBATCH --partition=gpu
+#SBATCH --job-name=N{n:03}K{k:03}{postfix}
+#SBATCH --partition={partition}
 #SBATCH --account=innovation
-#SBATCH --time=03:55:00
+#SBATCH --time=05:55:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --gpus-per-task=1
 #SBATCH --mem-per-cpu=1GB
+{gpus_per_task_line}
 
 module load 2023r1
 module load python
@@ -23,6 +27,7 @@ module load py-pandas
 
 python -m pip install --user jax optax ray[tune] flax PyDTMC
 
+
 cd compare_pve_mle
 srun python exp2.py {n} {k} {n_seeds}> out_{mkey}.log
 """
@@ -33,8 +38,18 @@ def gen_job(n, k):
     mkey = f"N{n:03}_K{k:03}"
     if not os.path.exists('jobs'):
         os.makedirs('jobs')
+    if random.random() < .10:
+        partition = 'gpu'
+        postfix = 'gpu'
+        gpus_per_task_line = gpus_per_task_one
+    else:
+        partition = 'compute'
+        postfix = 'cpu'
+        gpus_per_task_line = ''
+
+    text = template.format(mkey=mkey, n=n, k=k, n_seeds=n_seeds, partition=partition, postfix=postfix, gpus_per_task_line=gpus_per_task_line)
     with open(f'jobs/job_{mkey}.sh', 'w') as f:
-        f.write(template.format(mkey=mkey, n=n, k=k, n_seeds=n_seeds))
+        f.write(text)
 
 range_n = [8, 16, 32, 64, 128]
 range_k = [10, 30, 50, 70, 90, 104]
